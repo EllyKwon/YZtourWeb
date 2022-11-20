fe8ca469b3418d6f2383846140f4666cb2001623
EllyKwon <tok_o@naver.com>
2022-11-20 19:41:33 +0900

    Initial commit

---
+++ src/test/java/com/YZtour/Notice/YZtourWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YZtour.Notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YZtourWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
